9501bf3b8b91c5fd60cf44f0bfdd0d6d97e41ffc
unknown <petarostojic.vbps@gmail.com>
2024-12-25 11:53:43 +0100

    fixing err

--- src/components/InputMeterState.js
+++ src/components/InputMeterState.js
@@ -7,7 +7,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { confirmAlert } from 'react-confirm-alert';
 
 
-function InputMeterState(/* {apiUrl} */) {
+function InputMeterState({apiUrl}) {
     const [isMobile, setIsMobile] = useState(false);
     const [states, setStates] = useState([]);
     const [selectedRow, setSelectedRow] = useState(0);
